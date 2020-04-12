862f419eb14c2d4b1a2012e4446e3dd705cfb9ba
Lyubomir Kyorovski <KyorovskiLR@cardiff.ac.uk>
2020-04-12 18:12:53 +0300

    Added front-end libs manager Renamed foleder "routers" to "routes" Added a simple registration landing page

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const validator = require('./server/utils/validator');
 const data = require('./server/data')(config.connectionString, {validator});
 const app = require('./server/config/app')({data});
 const controllers = require('./server/controllers')({data});
-require('./server/routers')(app, controllers, {});
+require('./server/routes')(app, controllers, {});
 
 const server = app.listen(config.port, () => {
   console.log(`App running on localhost:${server.address().port}`);
