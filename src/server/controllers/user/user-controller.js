d384a0535114fac313045b8bd495dffd23ac092b
Lyubomir Kyorovski <KyorovskiLR@cardiff.ac.uk>
2020-04-17 03:18:10 +0300

    Added backend lofic for user registration Added cryptoutils

--- src/server/controllers/user/user-controller.js
+++ src/server/controllers/user/user-controller.js
@@ -2,10 +2,12 @@ module.exports = function(params) {
   const {
     data,
     validator,
+    cryptoUtils,
   } = params;
 
   /**
-   *
+   * Checks handles the request of checking whether a username is free
+   * Username is supplied as a path parameter
    *
    * @param {Express.Request} req
    * @param {Express.Response} res
@@ -23,7 +25,61 @@ module.exports = function(params) {
 
     return res.json({isFree: !exists});
   }
+
+  /**
+   * Registers a new user
+   *
+   * @param {Express.Request} req
+   * @param {Express.Response} res
+   * @param {Express.next} next
+   */
+  async function registerUser(req, res, next) {
+    const {
+      username,
+      publicKey,
+      spk,
+      otpks,
+    } = req.body;
+
+    if (!validator.validateString(username, 3, 30)) {
+      const error = new Error('Invalid username');
+      error.public = true;
+      return next(error);
+    }
+
+    try {
+      await cryptoUtils.openSignedEnvelope(
+          spk.envelope,
+          publicKey,
+      );
+    } catch (e) {
+      const error = new Error('Invalid signed pre-key signature!');
+      error.public = true;
+      return next(error);
+    }
+
+    let invalidOtpkId;
+    try {
+      for (const otpk of otpks) {
+        invalidOtpkId = otpk.id;
+        await cryptoUtils.openSignedEnvelope(
+            otpk.envelope,
+            publicKey,
+        );
+      };
+    } catch (e) {
+      const error =
+        new Error(`Invalid one-time pre-key with ID ${invalidOtpkId}!`);
+      error.public = true;
+      return next(error);
+    }
+
+    await data.createUser(username, publicKey, spk, otpks);
+
+    res.sendStatus(200);
+  }
   return {
     isUsernameFree,
+    registerUser,
   };
 };
