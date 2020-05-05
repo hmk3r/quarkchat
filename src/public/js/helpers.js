3e1459842db32520a7072f9343fe08ea83df198f
Lyubomir Kyorovski <KyorovskiLR@cardiff.ac.uk>
2020-05-05 13:02:17 +0300

    Added typed json serialisation

--- src/public/js/helpers.js
+++ src/public/js/helpers.js
@@ -48,3 +48,60 @@ function timeout(ms) {
 Date.prototype.toLocaleDateTimeString = function() {
   return moment(this).format('HH:mm   |   MMM DD YYYY')
 }
+
+const typedJSON = (() => {
+  const TYPE_FIELD = '__$$type'
+  const VALUE_FIELD = '__$$value'
+
+  function replacer(key, value) {
+    let tmpBuffer;
+    if (this[key] instanceof Date) {
+      return {
+        [TYPE_FIELD]: this[key].constructor.name,
+        [VALUE_FIELD]: this[key].toJSON()
+      }
+    } else if (value instanceof ArrayBuffer) {
+      tmpBuffer = value;
+    } else if (ArrayBuffer.isView(value)) {
+      tmpBuffer = value.buffer;
+    } else {
+      return value;
+    }
+
+    return {
+      [TYPE_FIELD]: value.constructor.name,
+      [VALUE_FIELD]: cryptoHelper.bufToBase64(tmpBuffer)
+    }
+  }
+
+  function reviver(key, value) {
+    if(!value[TYPE_FIELD] || !value[VALUE_FIELD]) {
+      return value;
+    }
+
+    if (value[TYPE_FIELD] === Date.name) {
+      return new Date(value[VALUE_FIELD]);
+    }
+
+    const tmpBuffer = cryptoHelper.base64ToBuf(value[VALUE_FIELD])
+    
+    if (value[TYPE_FIELD] === ArrayBuffer.name) {
+      return tmpBuffer;
+    }
+
+    return new window[value[TYPE_FIELD]](tmpBuffer);
+  }
+
+  function parse(string) {
+    return JSON.parse(string, reviver)
+  }
+
+  function stringify(object) {
+    return JSON.stringify(object, replacer)
+  }
+
+  return {
+    parse,
+    stringify
+  }
+})();
